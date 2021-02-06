27fed78810c57d9745fe27b2afe45d840d237bd5
Jared Butcher <jared.butcher1219@gmail.com>
2021-02-06 14:11:30 -0500

    server netobj children support

--- gameserver/netobj.py
+++ gameserver/netobj.py
@@ -2,15 +2,37 @@ import typing
 
 class NetObj:
     netObjs = {}
+    rootObjs = {}
     send = None
     netIdCounter = 1
 
-    def __init__(self, parent: typing.Optional[int] = 0, authority: typing.Optional[int] = 0):
+    @staticmethod
+    def find(name: str) -> typing.Union[NetObj, None]:
+        delimiter = name.find('/')
+        if delimiter:
+            objName = name[0:delimiter]
+            path = name[name.find('/') + 1:delimiter]
+            obj = NetObj.rootObjs.get(objName, None)
+            if obj:
+                return obj.findChild(path)
+        else:
+            return NetObj.rootObjs.get(name, None)
+
+    @staticmethod
+    def attachRootObj(obj: 'NetObj'):
+        if obj.parent:
+            obj.parent.children.pop(obj.name, None)
+        obj.parent = None
+        NetObj.rootObjs[obj.name] = obj
+
+    def __init__(self, name: str = '', authority: typing.Optional[int] = None):
         self.authority = authority
         self._id = NetObj.netIdCounter
-        self.parent = parent
         NetObj.netIdCounter += 1
         NetObj.netObjs[self.id] = self
+        self.parent = None
+        self.name = name if name else str(type(self)) + str(self.id)
+        self.children = {}
         self._cmds = {}
         for attrName in dir(self):
             attr = getattr(self, attrName)
@@ -20,6 +42,30 @@ class NetObj:
     @property
     def id(self):
         return self._id
+
+    def onStart(self):
+        pass
+
+    def onUpdate(self):
+        pass
+
+    def findChild(self, name: str) -> typing.Union[NetObj, None]:
+        delimiter = name.find('/')
+        if delimiter:
+            objName = name[0:delimiter]
+            path = name[name.find('/') + 1:delimiter]
+            obj = self.children.get(objName, None)
+            if obj:
+                return obj.findChild(path)
+        else:
+            return self.children.get(name, None)
+
+    def attachChild(self, child: 'NetObj'):
+        NetObj.rootObjs.pop(child.name, None)
+        if obj.parent:
+            obj.parent.children.pop(obj.name, None)
+        obj.parent = self
+        self.children[obj.name] = obj
 
     def rpcAll(self, procedure: str, *args):
         NetObj.send({'D': self.id, 'P': procedure, 'A': [*args]})
@@ -33,14 +79,22 @@ class NetObj:
             if cmd: cmd(*commandMsg['A'])
 
     def serialize(self, **kwargs) -> dict:
-        return {'D': 0, 'P': '__init__', 'A': [self.type, {'id': self.id, 'parent': self.parent, 'authority': self.authority, **kwargs}]}
+        return {'D': 0, 'P': '__init__', 'A': [self.type, 
+            {'id': self.id, 'name': self.name, 'parent': self.parent.id, 'authority': self.authority, 
+            'children': [child.id for child in self.children], **kwargs}]}
 
     def destroy(self):
+        if self.parent:
+           self.parent.children.pop(self.name, None)
+        else:
+            NetObj.rootObjs.pop(self.name, None)
+        self._destory() 
+        self.rpcAll('__del__')
+
+    def _destory(self):
         NetObj.netObjs.pop(self.id, None)
         objectsToRemove = []
-        for key, obj in NetObj.netObjs.items():
-            if obj.parent == self.id:
-                objectsToRemove.append(obj)
-        for obj in objectsToRemove:
-            obj.destory()
-        self.rpcAll('__del__')
+        for child in self.children.values():
+            child._delete()
+        self.children = None
+        
